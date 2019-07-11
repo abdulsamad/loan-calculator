9dafcb23ee586b71e0eec436c32f8fae45f1ea37
Sam <Samk8778@yahoo.com>
2019-07-11 18:53:38 +0530

    :bug: Fixed barchart showing wrong principal bug

--- webpack.config.js
+++ webpack.config.js
@@ -5,92 +5,104 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  devtool: 'eval',
-  entry: {
-    app: './src/js/main.js'
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/app.bundle.js'
-  },
-  module: {
-    rules: [{
-        test: /\.html$/,
-        exclude: '/node_modules/',
-        use: [{
-          loader: 'html-loader',
-          options: {
-            minimize: true
-          }
-        }]
-      },
-      {
-        test: /\.css$/,
-        exclude: '/node_modules/',
-        use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader',
-        }],
-      },
-      {
-        test: /\.scss$/,
-        exclude: '/node_modules/',
-        use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            }
-          }
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192,
-          },
-        }, ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {},
-        }, ],
-      },
-      {
-        test: /\.js$/,
-        exclude: '/node_modules/',
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  plugins: [
-    new HTMLWebPackPlugin({
-      template: 'src/index.html',
-      filename: './index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].bundle.css',
-    })
-  ],
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
-  externals: {
-    moment: 'moment'
-  },
-  devServer: {
-    contentBase: 'dist',
-    port: 8080,
-    watchContentBase: true
-  }
-}
+	devtool: 'eval',
+	entry: {
+		app: './src/js/main.js',
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/app.bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				exclude: '/node_modules/',
+				use: [
+					{
+						loader: 'html-loader',
+						options: {
+							minimize: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.css$/,
+				exclude: '/node_modules/',
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+					},
+				],
+			},
+			{
+				test: /\.scss$/,
+				exclude: '/node_modules/',
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|jpg|gif)$/i,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 8192,
+						},
+					},
+				],
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {},
+					},
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: '/node_modules/',
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	plugins: [
+		new HTMLWebPackPlugin({
+			template: 'src/index.html',
+			filename: './index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].bundle.css',
+		}),
+	],
+	optimization: {
+		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+	},
+	externals: {
+		moment: 'moment',
+	},
+	devServer: {
+		contentBase: 'dist',
+		port: 8080,
+		watchContentBase: true,
+	},
+};
