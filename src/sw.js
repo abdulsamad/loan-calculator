dea232f110d92718bee3f27176069691af1308a9
Abdul Samad <Samk8778@yahoo.com>
2020-04-30 20:43:03 +0530

    :sparkles: Added caching strategy

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
+	self.skipWaiting();
+
+	workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new StaleWhileRevalidate());
+
+	workbox.routing.registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate());
+
 	workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 }
