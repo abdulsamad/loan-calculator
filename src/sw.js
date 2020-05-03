fbf3f4bbde0cda83d3dbca61156631ca448711d4
Abdul Samad <Samk8778@yahoo.com>
2020-05-03 16:33:40 +0530

    :construction: Added workbox set config

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,8 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
+	workbox.setConfig({ modulePathPrefix: 'workbox-v5.1.2' });
+
 	self.skipWaiting();
 
 	workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new StaleWhileRevalidate());
