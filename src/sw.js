ee92cca53119f8bd306707866584c8bc999cea4a
Abdul Samad <Samk8778@yahoo.com>
2020-05-03 17:26:00 +0530

    :construction: Updated service worker

--- src/sw.js
+++ src/sw.js
@@ -1,13 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 if (workbox) {
-	workbox.setConfig({ modulePathPrefix: 'workbox-v5.1.2' });
-
 	self.skipWaiting();
+
+	workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 	workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/, new StaleWhileRevalidate());
 
 	workbox.routing.registerRoute(/\.(?:js|css)$/, new StaleWhileRevalidate());
-
-	workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 }
